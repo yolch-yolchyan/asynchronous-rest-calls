1a7383f807af8907a55ca150165b019cae67f525
artur-yolchyan-at-405098022678 <artur.yolchyan.1990@gmail.com>
2020-02-12 20:29:52 +0100

    setting up the project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Sample Task
+
+## Prerequisites to Run the Application
+```
+- java 11
+- maven 3.5.5+
+```
+
+## How to Run the Application
+1. go to the root folder of the project
+2. execute `mvn clean package`
+3. execute `java -jar target/asynchronous-rest-calls-0.0.1-SNAPSHOT.jar`
---
+++ src/main/java/com/coding/challenge/AsynchronousRestCallsApplication.java
@@ -0,0 +1,13 @@
+package com.coding.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynchronousRestCallsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynchronousRestCallsApplication.class, args);
+	}
+
+}
